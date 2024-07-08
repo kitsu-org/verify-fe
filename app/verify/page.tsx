b7f485216a26ef9043ce85a80f07b3e842426f78
Jesse Wierzbinski <contact@cpluspatch.com>
2024-07-08 07:41:53 +0200

    feat: :sparkles: Add button to start verification

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,5 +1,4 @@
 import { ThemeToggle } from "@/components/theme-trigger";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import {
     Card,
@@ -9,22 +8,13 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 
-import { Verified } from "lucide-react";
-
-export default function Home() {
+export default function Verify() {
     return (
         <div className="flex min-h-screen w-full flex-col relative">
             <div className="absolute top-4 right-4 z-10 hidden md:block">
                 <ThemeToggle />
             </div>
             <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 max-w-2xl mx-auto w-full">
-                <Alert>
-                    <Verified className="h-4 w-4" />
-                    <AlertTitle>Welcome to KitsuVerify!</AlertTitle>
-                    <AlertDescription>
-                        We need to make sure you're really an adult.
-                    </AlertDescription>
-                </Alert>
                 <Card className="relative">
                     <CardHeader>
                         <CardTitle>Account Lockdown</CardTitle>
